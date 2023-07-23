7828698a499a823949885b3a457a809f5357161b
rakaar <rka87338@gmail.com>
2023-07-23 13:03:44 +0530

    parallel prescrape abstracts end point POST added

--- server.js
+++ server.js
@@ -3,16 +3,24 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 const port = 3000;
 
 // Enable CORS for all routes
 app.use(cors());
+// Middleware for parsing bodies from URL.
+app.use(bodyParser.urlencoded({ extended: true }));
+// Middleware for parsing json
+app.use(bodyParser.json());
+
 
 app.get('/', async (req, res) => {
     try {
         const pubmed_link = req.query.link;
+        // Your code here
         const data = await scrapePubmed(pubmed_link);
         res.json(data);
     } catch (err) {
@@ -20,7 +28,27 @@ app.get('/', async (req, res) => {
     }
 });
 
+app.post('/prescrape', async (req, res) => {
+    try {
+        const pubmedLinks = req.body;
+        const allNcbiLinks = pubmedLinks.map(item => modifyNcbiLink(item.PubMedLink));
+        const ncbi2pubmedLinks = allNcbiLinks.slice(0,Math.min(50, allNcbiLinks.length));
+        
+        const promises = ncbi2pubmedLinks.map(scrapePubmed);
+        const results = await Promise.all(promises); 
+
+        res.json(results)
+    } catch (err) {
+        res.status(500).send(`Error: ${err.message}`);
+    }
+});
+
+
 async function scrapePubmed(pubmed_link) {
+
+    if (pubmed_link === '') {
+        return {}
+    }
     // Send a request to the website
     const response = await axios.get(pubmed_link);
     const html = response.data;
@@ -40,6 +68,29 @@ async function scrapePubmed(pubmed_link) {
     
     // Return the data as an object
     return {title, authors, abstract};
+}
+
+function modifyNcbiLink(originalLink) {
+    // function to modify ncbi link to pubmed link
+
+    if (originalLink === '') {
+        return '';
+    }
+    let url = new URL(originalLink);
+
+    // Check if the hostname includes "ncbi.nlm.nih.gov"
+    if (url.hostname.includes("ncbi.nlm.nih.gov")) {
+        let list_uids = url.searchParams.get("list_uids");
+
+        // Ensure list_uids param exists and is a valid number
+        if (list_uids && !isNaN(list_uids)) {
+            // Construct the new link
+            return `https://www.pubmed.ncbi.nlm.nih.gov/${list_uids}`;
+        }
+    }
+
+    // If conditions aren't met, return the original link
+    return originalLink;
 }
 
 app.listen(port, () => {
